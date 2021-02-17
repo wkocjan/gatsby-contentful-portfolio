c8bc7bfe73cf1d054974a708bf63e52d5cccfb3c
nikmar13 <53223543+nikmar13@users.noreply.github.com>
2021-02-17 22:58:55 +0200

    add cloudfront pulumi config

--- infrastructure/index.js
+++ infrastructure/index.js
@@ -27,4 +27,48 @@ const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     })),
 });
 
+
+
 exports.bucketName = bucket.id;
+
+
+const s3OriginId = "myS3Origin";
+const s3Distribution = new aws.cloudfront.Distribution("s3Distribution", {
+    origins: [{
+        domainName: bucket.bucketRegionalDomainName,
+        originId: s3OriginId,
+    }],
+    enabled: true,
+    defaultRootObject: "index.html",
+
+    defaultCacheBehavior: {
+        allowedMethods: [
+            "GET",
+            "HEAD",
+        ],
+        cachedMethods: [
+            "GET",
+            "HEAD",
+        ],
+        targetOriginId: s3OriginId,
+        viewerProtocolPolicy: "allow-all",
+        minTtl: 0,
+        defaultTtl: 3600,
+        maxTtl: 86400,
+    },
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "whitelist",
+            locations: [
+                "UA",
+                "US",
+                "PL",
+                "DE",
+            ],
+        },
+    },
+    priceClass: "PriceClass_100",
+    viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+    },
+});
