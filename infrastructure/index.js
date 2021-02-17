e40d815136a1df0e968d37bcb96366a19ca22409
nikmar13 <53223543+nikmar13@users.noreply.github.com>
2021-02-17 20:44:51 +0200

    add yaml for contentful publish

--- infrastructure/index.js
+++ infrastructure/index.js
@@ -1,6 +1,6 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-const aws = require("@pulumi/aws");
+const aws = require ("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
 const bucket = new aws.s3.Bucket("static-website-ALPACKED",
@@ -12,5 +12,19 @@ const bucket = new aws.s3.Bucket("static-website-ALPACKED",
     },
 });
 
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: bucket.id,
+    policy: pulumi.all([bucket.arn, bucket.arn]).apply(([bucketArn, bucketArn1]) => JSON.stringify({
+        Version: "2012-10-17",
+        Id: "MYBUCKETPOLICY",
+        Statement: [{
+            Sid: "PublicReadGetObject",
+            Effect: "Allow",
+            Principal: "*",
+            Action: "s3:GetObject",
+            Resource: `${bucketArn1}/*`,
+        }],
+    })),
+});
 
 exports.bucketName = bucket.id;
