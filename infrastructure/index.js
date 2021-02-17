17a6cbdd5b0cb99c086cfd6e7520f1d9b0f66ee7
nikmar13 <53223543+nikmar13@users.noreply.github.com>
2021-02-17 18:13:47 +0200

    config s3 like static website

--- infrastructure/index.js
+++ infrastructure/index.js
@@ -3,7 +3,15 @@ const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
-const bucket = new aws.s3.Bucket("static-website-ALPACKED");
+const bucket = new aws.s3.Bucket("static-website-ALPACKED",
+{
+    bucket: config.targetDomain,
+    acl: "public-read",
+    website: {
+        indexDocument: "index.html",
+        errorDocument: "404.html",
+    },
+});
 
 
 exports.bucketName = bucket.id;
