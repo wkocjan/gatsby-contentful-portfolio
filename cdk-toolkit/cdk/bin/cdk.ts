b84d4baa06853dec6f88dc146d4cafbe8b911e03
Vlad Taushan <vlados1074@gmail.com>
2021-02-20 23:17:51 +0200

    commit on 20.02.21

---
+++ cdk-toolkit/cdk/bin/cdk.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+new CdkStack(app, 'CdkStack', {
+   env: {
+       region: "us-east-1"
+   }
+});
