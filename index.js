b135022e26d70187a0b12d34f4b9d68ae11d4df0
emocry-rgb2 <kreept.sama@gmail.com>
2021-02-23 00:43:30 +0300

    added

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
 exports.bucketName = bucket.id;
+
+//other
+const fs = require("fs");
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket,
+    content: fs.readFileSync("site/index.html").toString(),
+});
