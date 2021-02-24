d081461bde0b1e5d377644d883456a6c2472226b
vlad <vlados1074@gmail.com>
2021-02-24 14:14:11 +0200

    24.02 new infra

---
+++ pulumi/index.ts
@@ -0,0 +1,128 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.Bucket("bucket", {
+    acl: "private",
+    tags: {
+        Name: "My bucket",
+    },
+});
+const s3OriginId = "myS3Origin";
+const s3Distribution = new aws.cloudfront.Distribution("s3Distribution", {
+    origins: [{
+        domainName: bucket.bucketRegionalDomainName,
+        originId: s3OriginId,
+        s3OriginConfig: {
+            originAccessIdentity: "origin-access-identity/cloudfront/ABCDEFG1234567",
+        },
+    }],
+    enabled: true,
+    isIpv6Enabled: true,
+    comment: "Some comment",
+    defaultRootObject: "index.html",
+    loggingConfig: {
+        includeCookies: false,
+        bucket: "mylogs.s3.amazonaws.com",
+        prefix: "myprefix",
+    },
+    aliases: [
+        "mysite.example.com",
+        "yoursite.example.com",
+    ],
+    defaultCacheBehavior: {
+        allowedMethods: [
+            "DELETE",
+            "GET",
+            "HEAD",
+            "OPTIONS",
+            "PATCH",
+            "POST",
+            "PUT",
+        ],
+        cachedMethods: [
+            "GET",
+            "HEAD",
+        ],
+        targetOriginId: s3OriginId,
+        forwardedValues: {
+            queryString: false,
+            cookies: {
+                forward: "none",
+            },
+        },
+        viewerProtocolPolicy: "allow-all",
+        minTtl: 0,
+        defaultTtl: 3600,
+        maxTtl: 86400,
+    },
+    orderedCacheBehaviors: [
+        {
+            pathPattern: "/content/immutable/*",
+            allowedMethods: [
+                "GET",
+                "HEAD",
+                "OPTIONS",
+            ],
+            cachedMethods: [
+                "GET",
+                "HEAD",
+                "OPTIONS",
+            ],
+            targetOriginId: s3OriginId,
+            forwardedValues: {
+                queryString: false,
+                headers: ["Origin"],
+                cookies: {
+                    forward: "none",
+                },
+            },
+            minTtl: 0,
+            defaultTtl: 86400,
+            maxTtl: 31536000,
+            compress: true,
+            viewerProtocolPolicy: "redirect-to-https",
+        },
+        {
+            pathPattern: "/content/*",
+            allowedMethods: [
+                "GET",
+                "HEAD",
+                "OPTIONS",
+            ],
+            cachedMethods: [
+                "GET",
+                "HEAD",
+            ],
+            targetOriginId: s3OriginId,
+            forwardedValues: {
+                queryString: false,
+                cookies: {
+                    forward: "none",
+                },
+            },
+            minTtl: 0,
+            defaultTtl: 3600,
+            maxTtl: 86400,
+            compress: true,
+            viewerProtocolPolicy: "redirect-to-https",
+        },
+    ],
+    priceClass: "PriceClass_200",
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "whitelist",
+            locations: [
+                "US",
+                "CA",
+                "GB",
+                "DE",
+            ],
+        },
+    },
+    tags: {
+        Environment: "production",
+    },
+    viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+    },
+});
