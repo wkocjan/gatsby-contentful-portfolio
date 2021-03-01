879a52fb81a92ad312403bbee322365ec7c021e7
Oleh <leon6871@gmail.com>
2021-03-01 18:02:23 +0200

    Update Pulumi config

--- pulumi/index.js
+++ pulumi/index.js
@@ -1,10 +1,51 @@
 "use strict";
-const pulumi = require("@pulumi/pulumi");
+
 const aws = require("@pulumi/aws");
-const awsx = require("@pulumi/awsx");
+const pulumi = require("@pulumi/pulumi");
+const mime = require("mime");
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// Create a bucket and expose a website index document
+let siteBucket = new aws.s3.Bucket("s3-website-bucket", {
+    website: {
+        indexDocument: "index.js",
+    },
+});
 
-// Export the name of the bucket
-exports.bucketName = bucket.id;
+let siteDir = "../src/pages"; // directory for content files
+
+// For each file in the directory, create an S3 object stored in `siteBucket`
+for (let item of require("fs").readdirSync(siteDir)) {
+    let filePath = require("path").join(siteDir, item);
+    let object = new aws.s3.BucketObject(item, {
+        bucket: siteBucket,                               // reference the s3.Bucket object
+        source: new pulumi.asset.FileAsset(filePath),     // use FileAsset to point to a file
+        contentType: mime.getType(filePath) || undefined, // set the MIME type of the file
+    });
+}
+
+// Create an S3 Bucket Policy to allow public read of all objects in bucket
+function publicReadPolicyForBucket(bucketName) {
+    return {
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Principal: "*",
+            Action: [
+                "s3:GetObject"
+            ],
+            Resource: [
+                `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
+            ]
+        }]
+    };
+}
+
+// Set the access policy for the bucket so all objects are readable
+let bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: siteBucket.bucket, // refer to the bucket created earlier
+    policy: siteBucket.bucket.apply(publicReadPolicyForBucket) // use output property `siteBucket.bucket`
+});
+
+// Stack exports
+exports.bucketName = siteBucket.bucket;
+exports.websiteUrl = siteBucket.websiteEndpoint;
