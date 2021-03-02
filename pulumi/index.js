1b1e2f66c285617bada479389cd27e47dbbd9b07
kraken <33196555+kraken194@users.noreply.github.com>
2021-03-02 16:54:39 +0200

    Update index.js

--- pulumi/index.js
+++ pulumi/index.js
@@ -58,40 +58,9 @@ const config = {
 };
 
 
-function crawlDirectory(dir: string, f: (_: string) => void) {
-    const files = fs.readdirSync(dir);
-    for (const file of files) {
-        const filePath = `${dir}/${file}`;
-        const stat = fs.statSync(filePath);
-        if (stat.isDirectory()) {
-            crawlDirectory(filePath, f);
-        }
-        if (stat.isFile()) {
-            f(filePath);
-        }
-    }
-}
-
 const webContentsRootPath = path.join(process.cwd(), config.pathToWebsiteContents);
 console.log("Syncing contents from local disk at", webContentsRootPath);
-crawlDirectory(
-    webContentsRootPath,
-    (filePath: string) => {
-        const relativeFilePath = filePath.replace(webContentsRootPath + "/", "");
-        const contentFile = new aws.s3.BucketObject(
-            relativeFilePath,
-            {
-                key: relativeFilePath,
 
-                acl: "public-read",
-                bucket: contentBucket,
-                contentType: mime.getType(filePath) || undefined,
-                source: new pulumi.asset.FileAsset(filePath),
-            },
-            {
-                parent: contentBucket,
-            });
-    });
 
 const distributionArgs: aws.cloudfront.DistributionArgs = {
     enabled: true,
