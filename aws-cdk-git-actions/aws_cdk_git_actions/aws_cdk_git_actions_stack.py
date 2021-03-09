f29c0f64f9d618502b4d11919fdee89bd9e4a325
Serhii Tkachenko <60434659+serhiitk@users.noreply.github.com>
2021-03-09 16:14:42 +0200

    Fix install python dependencies

--- aws-cdk-git-actions/aws_cdk_git_actions/aws_cdk_git_actions_stack.py
+++ aws-cdk-git-actions/aws_cdk_git_actions/aws_cdk_git_actions_stack.py
@@ -20,24 +20,24 @@ class AwsCdkGitActionsStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # Create AWS S3 Bucket
-        bucket = s3.Bucket(self, 
-            id = AWS_S3_BUCKET_NAME, 
-            bucket_name = AWS_S3_BUCKET_NAME, 
-            public_read_access = True, 
+        bucket = s3.Bucket(self,
+            id = AWS_S3_BUCKET_NAME,
+            bucket_name = AWS_S3_BUCKET_NAME,
+            public_read_access = True,
             website_index_document = "index.html",
             removal_policy = core.RemovalPolicy.DESTROY,
-            auto_delete_objects = True,
+            # auto_delete_objects = True,
         )
-        
+
         source_config = cloudfront.SourceConfiguration(
             s3_origin_source = cloudfront.S3OriginConfig(
-                s3_bucket_source = bucket    
+                s3_bucket_source = bucket
             ),
             behaviors = [cloudfront.Behavior(is_default_behavior=True)]
         )
-        
+
         # Create CloudFront distribution
-        distribution = cloudfront.CloudFrontWebDistribution(self, 
+        distribution = cloudfront.CloudFrontWebDistribution(self,
             id = AWS_CLOUDFRONT_DISTRIBUTION_NAME,
             origin_configs = [source_config]
         )
